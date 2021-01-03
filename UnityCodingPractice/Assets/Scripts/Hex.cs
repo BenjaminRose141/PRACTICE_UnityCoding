1382e3fdc0aeb7d3b88fc8fffd2487b40e26e097
Benjamin Rose <rose.benjamin@t-online.de>
2021-01-03 17:29:50 +0100

    Created basis of HexMap Generation (HexMap Generator, Hex class, tiles)

---
+++ UnityCodingPractice/Assets/Scripts/Hex.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Holds information of a Hex tile (grid position, world position, size, neighbours etc.).
+/// </summary>
+
+public class Hex
+{
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = -(q + r);
+    }
+
+    // Q + R + S = 0
+    // S = -(Q + R)
+
+    public readonly int Q;  //Column
+    public readonly int R;  //Row
+    public readonly int S;
+
+    static readonly float widthMultiplier = Mathf.Sqrt(3f) / 2f;
+
+    //Returns world space position of this hex
+    public Vector3 Position()
+    {
+        float radius = 0.5f;
+        float height = radius * 2f;
+        float width = widthMultiplier * height;
+
+        float horizontal = width;
+        float vertical = height * 0.75f;
+
+        //Debug.Log("Info: rad: " + radius + " , height: " + height + " width: " + width + " hor: " + horizontal + " vert: " + vertical);
+
+        return new Vector3(horizontal * (this.Q + this.R/2f), 0f, vertical * this.R);
+    }
+}
---
+++ UnityCodingPractice/Assets/Scripts/HexMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class HexMap : MonoBehaviour
+{
+    [SerializeField] GameObject hexPrefab;
+
+    private void Start()
+    {
+        GenerateHexMap();
+    }
+
+    [Button]
+    private void GenerateHexMap()
+    {
+        for(int column = 0; column < 10; column++)
+        {
+            for(int row = 0; row < 10; row++)
+            {
+                Hex h = new Hex(column, row);
+                Instantiate(hexPrefab, h.Position(), Quaternion.identity, this.transform);
+            }
+        }
+
+        Debug.Log("Generated Hex Map");
+    }
+}
